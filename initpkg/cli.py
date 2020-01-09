acb3ccc3447d9e53e99bd7ac4a5241c1bde22aa2
benjamin.wang <benjamin.wang@nl.abnamro.com>
2020-01-09 17:09:19 +0100

    Reformat some docstrings

--- initpkg/cli.py
+++ initpkg/cli.py
@@ -1,9 +1,17 @@
+"""
+Main CLI entry point.
+"""
 import argparse
 import os
 import shutil
 
 
-def create_template(package_name):
+def create_package_dir(package_name):
+    """
+    Main function to create a basic python package directory.
+
+    :param str package_name: the intended package name
+    """
     template_pkg_name = os.path.join(os.path.dirname(__file__), "template")
     new_pkg_name = os.path.join(os.getcwd(), package_name)
     shutil.copytree(template_pkg_name, package_name)
@@ -11,8 +19,11 @@ def create_template(package_name):
 
 
 def main():
+    """
+    Main runner for CLI.
+    """
     parser = argparse.ArgumentParser(description='create python package skeleton')
     parser.add_argument('name', help='name of the package')
     args = parser.parse_args()
     if "name" in args:
-        create_template(args.name)
+        create_package_dir(args.name)
